f520dfd61fd22843ecdbabfca8c92e08217fac33
EC8827K <luisgoemon@gmail.com>
2023-10-11 14:30:48 -0500

    se agrega entidades

---
+++ src/main/java/com/foodfinder/entities/menu.java
@@ -0,0 +1,59 @@
+package com.foodfinder.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "menu")
+public class menu implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "id_menu")
+	private Integer id;
+	
+	@Basic(optional = false)
+	@Column(name = "nombre")
+	private String name;
+	
+	@Basic(optional = false)
+	@Column(name = "descripcion")
+	private String description;
+	
+	@Basic(optional = false)
+	@Column(name = "precio")
+	private float price;
+	
+	@Basic(optional = false)
+	@Column(name = "disponibilidad")
+	private Integer availability;
+	
+	@JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
+	@OneToOne
+	private menuCategory categoria;
+	
+	/*@JoinColumn(name = "id_restaurante", referencedColumnName = "id_restaurante")
+	@OneToOne
+	private Restaurant restaurant; */
+}
---
+++ src/main/java/com/foodfinder/entities/menuCategory.java
@@ -0,0 +1,46 @@
+package com.foodfinder.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categoria_menu")
+@EntityListeners(AuditingEntityListener.class)
+public class menuCategory implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "id_categoria")
+	private Integer id;
+	
+	@Basic(optional = false)
+	@Column(name = "nombre ")
+	private String name;
+	
+	@Basic(optional = false)
+	@Column(name = "descripcion")
+	private String description ;
+	
+}
