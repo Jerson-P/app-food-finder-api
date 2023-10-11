582e6a93fcf17ec4cc56c10499ede7ee02abeb92
Jerson-P <bolanosy8@gmail.com>
2023-10-11 11:35:12 -0500

    Inicial commit

---
+++ src/main/java/com/foodfinder/FoodFinderApplication.java
@@ -0,0 +1,13 @@
+package com.foodfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodfinder/FoodFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
