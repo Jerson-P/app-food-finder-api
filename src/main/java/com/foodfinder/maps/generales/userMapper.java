41c39919bb8d9245a0cd9bb046a8bb83e10d6906
DESKTOP-MCR4TPC\ADMIN <luisgoemon@gmail.com>
2023-10-11 14:05:56 -0600

    Implementación de DTO para la respuesta de serivicios, dto, mapper y repositorio de User

--- src/main/java/com/foodfinder/maps/generales/userMapper.java
+++ src/main/java/com/foodfinder/maps/generales/userMapper.java
@@ -1,9 +1,31 @@
 package com.foodfinder.maps.generales;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.foodfinder.dtos.userDTO;
+import com.foodfinder.entities.user;
+
+/**
+ * @author nicope
+ * @version 1.0
+ * 
+ *          Clase Mapper de tipo (user) que permite mapear los datos un objeto de tipo 
+ *          (userDTO) a uno de tipo (user) o viceversa.
+ *          
+ *          Esto ayuda a mantener el código limpio y mantenible, ya que separa las 
+ *          preocupaciones de las diferentes capas de la aplicación.
+ * 
+ */
 
 @Mapper
 public interface userMapper {
 
+	userMapper INSTANCE = Mappers.getMapper(userMapper.class);
 	
+	userDTO entityToDto(user entity);
+	
+	@InheritInverseConfiguration
+	user dtoToEntity(userDTO dto);
 }
