da4bddbe342cc387131ea5854b6d3964b64c7d68
Jerson-P <bolanosy8@gmail.com>
2023-10-12 12:36:10 -0500

    Se agrega el Get All a Restaurant Detail

---
+++ src/main/java/com/foodfinder/service/IRestaurantDetailService.java
@@ -0,0 +1,18 @@
+package com.foodfinder.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.foodfinder.dtos.ResponseDTO;
+
+/**
+ * @author Jerson Peña
+ * @version 1.0
+ * 
+ *          
+ *          
+ */
+public interface IRestaurantDetailService {
+
+	public ResponseEntity<ResponseDTO> getRestaurantsDetail();
+	
+}
