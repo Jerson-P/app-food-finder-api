4ff51463949d51d309bc73f5e72d8af6b23be948
nicolasback04 <nicope2004@gmail.com>
2023-10-12 11:30:08 -0600

    Ajuste modificación de Identity Id_Usuario

--- src/main/java/com/foodfinder/entities/User.java
+++ src/main/java/com/foodfinder/entities/User.java
@@ -48,7 +48,7 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Basic(optional = false)
 	@Column(name = "id_usuario")
 	private Integer id;
