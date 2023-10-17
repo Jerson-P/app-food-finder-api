9a81cdc5135cd5f4babf03973e8d3044e0ddc939
nicolasback04 <nicope2004@gmail.com>
2023-10-17 10:51:24 -0600

    Service Delete User ID

--- src/main/java/com/foodfinder/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/foodfinder/serviceImpl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.foodfinder.serviceImpl;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,44 +29,52 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UserServiceImpl implements IUserService{
-	
+public class UserServiceImpl implements IUserService {
+
 	private final UserRepository userRepository;
-	
+
 	/**
 	 * Método que permite obtener todos los ususarios .
 	 */
 	@Override
-	public ResponseEntity<ResponseDTO> obtenerUsuarios(){
+	public ResponseEntity<ResponseDTO> obtenerUsuarios() {
 		log.info("Inicio método Obtener Users");
-		return new ResponseEntity<ResponseDTO>(Utils.mapearRespuesta(Constants.CONSULTA_EXITOSAMENTE,
-				HttpStatus.OK.value(), UserMapper.INSTANCE.beanListToDtoList(this.userRepository.findAll())),
-				HttpStatus.OK);
+
+		ResponseDTO responseDTO = ResponseDTO.builder().statusCode(HttpStatus.OK.value())
+				.message(Constants.CONSULTA_EXITOSAMENTE)
+				.objectResponse(UserMapper.INSTANCE.beanListToDtoList(this.userRepository.findAll()))
+				.count(this.userRepository.count()).build();
+
+		return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
 	}
-	
+
 	/**
 	 * Método que permite optener los usuario por Id.
 	 */
-	
+
 	@Override
 	public ResponseEntity<ResponseDTO> findUserById(Integer id) {
 		log.info("Inicio metodo para obtener usuario por id");
-		return new ResponseEntity<ResponseDTO>(Utils.mapearRespuesta(Constants.CONSULTA_EXITOSAMENTE,
-				HttpStatus.OK.value(), this.userRepository.findById(id)),
-				HttpStatus.OK);
+
+		ResponseDTO responseDTO = ResponseDTO.builder().statusCode(HttpStatus.OK.value())
+				.message(Constants.CONSULTA_EXITOSAMENTE)
+				.objectResponse(UserMapper.INSTANCE.beanListToDtoList(this.userRepository.findAll()))
+				.count(this.userRepository.count()).build();
+
+		return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
 	}
-	
+
 	/**
 	 * Método que permite registrar un usuario.
 	 */
 	@Override
-	public ResponseEntity<ResponseDTO> save(UserDTO user){
+	public ResponseEntity<ResponseDTO> save(UserDTO user) {
 		boolean permiteGuardar = false;
 		List<User> listUsuario = this.userRepository.findAll();
-		System.out.println("guardar usuario -> "+ user);
-		if(Objects.nonNull(user.getUser())) {
-			for(User u : listUsuario) {
-				if(Objects.nonNull(listUsuario)) {
+		System.out.println("guardar usuario -> " + user);
+		if (Objects.nonNull(user.getUser())) {
+			for (User u : listUsuario) {
+				if (Objects.nonNull(listUsuario)) {
 					permiteGuardar = true;
 				} else {
 					permiteGuardar = false;
@@ -75,33 +82,43 @@ public class UserServiceImpl implements IUserService{
 				}
 			}
 		}
-		if(permiteGuardar) {
+		if (permiteGuardar) {
 			log.info("Inicio método de guardar usuario");
-			
-			/*if(user.getId() != null) {
-				Optional<User> usuarioTemp = this.userRepository.findById(user.getId());
-				
-				if(!usuarioTemp.get().getPassword().equals(user.getPassword())) {
-					user.setPassword(user.getPassword());
-					this.userRepository.save(UserMapper.INSTANCE.dtoToEntity(user));
-					return new ResponseEntity<ResponseDTO>(Utils.mapearRespuesta(Constants.ACTUALIZADO_EXITOSAMENTE, HttpStatus.CREATED.value()), HttpStatus.CREATED);
-				} else {
-					String clave = user.getPassword();
-					user.setPassword(clave);
-					this.userRepository.save(UserMapper.INSTANCE.dtoToEntity(user));
-					log.info("Fin del metodo editar usuario");
-					return new ResponseEntity<ResponseDTO>(Utils.mapearRespuesta(Constants.ACTUALIZADO_EXITOSAMENTE, HttpStatus.CREATED.value()), HttpStatus.CREATED);
-				}
-			}*/
+
 			user.setId(user.getId());
 			user.setPassword(user.getPassword());
 			this.userRepository.save(UserMapper.INSTANCE.dtoToEntity(user));
 			log.info("Fin metodo de guardar usuario");
-			return new ResponseEntity<ResponseDTO>(Utils.mapearRespuesta(Constants.GUARDADO_EXITOSAMENTE, HttpStatus.CREATED.value()),
-					HttpStatus.CREATED);
+
+			ResponseDTO responseDTO = ResponseDTO.builder().statusCode(HttpStatus.OK.value())
+					.message(Constants.GUARDADO_EXITOSAMENTE)
+					.objectResponse(UserMapper.INSTANCE.beanListToDtoList(this.userRepository.findAll()))
+					.count(this.userRepository.count()).build();
+
+			return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
 		} else {
-			return new ResponseEntity<ResponseDTO>(Utils.mapearRespuesta(Constants.USUARIO_NO_PUEDE_GUARDAR, HttpStatus.BAD_REQUEST.value()),
-					HttpStatus.BAD_REQUEST);
+
+			ResponseDTO responseDTO = ResponseDTO.builder().statusCode(HttpStatus.ACCEPTED.value())
+					.message(Constants.USUARIO_NO_PUEDE_GUARDAR)
+					.objectResponse(UserMapper.INSTANCE.beanListToDtoList(this.userRepository.findAll()))
+					.count(this.userRepository.count()).build();
+
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDTO);
+		}
+	}
+
+	@Override
+	public ResponseEntity<ResponseDTO> deleteUser(Integer id) {
+		try {
+			log.info("Inicio método de eliminar Usuario");
+			this.userRepository.deleteById(id);
+
+			return new ResponseEntity<ResponseDTO>(
+					Utils.mapearRespuesta(Constants.ELIMINADO_EXITOSAMENTE, HttpStatus.OK.value()), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<ResponseDTO>(
+					Utils.mapearRespuesta(Constants.NO_SE_PUEDE_ELIMINAR, HttpStatus.ACCEPTED.value()),
+					HttpStatus.ACCEPTED);
 		}
 	}
 
