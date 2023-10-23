53dae42eb1fc44b8f1f3eda8e415b2f3db6f6072
Jerson-P <bolanosy8@gmail.com>
2023-10-23 10:17:09 -0500

    Se crea la funcion del Put en el controllador de restaurantDetail

--- src/main/java/com/foodfinder/controllers/RestaurantDetailController.java
+++ src/main/java/com/foodfinder/controllers/RestaurantDetailController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.foodfinder.dtos.MenuCategoryDTO;
 import com.foodfinder.dtos.ResponseDTO;
 import com.foodfinder.dtos.RestaurantDetailDTO;
 import com.foodfinder.serviceImpl.RestaurantDetailServiceImpl;
@@ -104,4 +106,19 @@ public class RestaurantDetailController {
 	public ResponseEntity<ResponseDTO> getMenuId(@PathVariable Integer id) {
 		return this.RestaurantDetailService.findRestaurantDetailById(id);
 	}
+	
+	@Operation(summary = "Operación que permite actualizar una el detalle de un restaurante a partir de un id")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = com.foodfinder.dtos.ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<ResponseDTO> update(@PathVariable Integer id, @RequestBody RestaurantDetailDTO restauranDetail) {
+        return this.RestaurantDetailService.update(id, restauranDetail);
+    }
 }
